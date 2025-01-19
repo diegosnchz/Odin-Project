5b98ee7a9ada421940c82f75cb4f7e1af8aab237
diegosnchz <diegovscodee@gmail.com>
2025-01-19 11:51:18 +0100

    hecho el etch to sketch

--- salvacion/dom/sketch/script.js
+++ salvacion/dom/sketch/script.js
@@ -1,21 +1,28 @@
-//crear divs aquí
 let container = document.querySelector(".container");
 
-let createDivs = () => {
+
+let nSize = () => {
+  return prompt("De cuanto quieres el tablero (NxN)?");
+ }
+
+let createDivs = (n) => {
   container.style.display = "grid";
-  container.style.gridTemplateColumns = "repeat(16, 30px)";
-  container.style.gridTemplateRows = "repeat(16, 30px)";
-  
-  for (let i = 0; i < 256; i++){
+  container.style.gridTemplateColumns = `repeat(${n}, 30px)`
+  container.style.gridTemplateRows = `repeat(${n}, 30px)`;
+
+  for (let i = 0; i < n* n; i++) {
     let squareDivs = document.createElement("div");
-    squareDivs.class = "divsRojo";
-    squareDivs.style.border = "1px solid black"; //borde pa que se vea
+    squareDivs.style.border = "2px solid black";  //esto es para ponerle borde
+    squareDivs.classList.add("divsRojo"); // Añadir la clase para el hover
+
+    // Añadir el evento mouseover a cada div
+    squareDivs.addEventListener("mouseover", () => {
+      squareDivs.style.backgroundColor = "red"; // Cambiar color de fondo al pasar el ratón
+    });
+
     container.appendChild(squareDivs);
   }
 }
 
-container.addEventListener("mouseover", (e) => {
-  squareDivs.style.backgroundcolor = "red";
-})
-
-createDivs();
+let n = nSize();
+createDivs(n);
