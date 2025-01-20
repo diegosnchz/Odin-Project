0dc02c2f69242fef0d3c320496fdc496045a7c69
diegosnchz <diegovscodee@gmail.com>
2025-01-20 09:46:34 +0100

    haciendo cosas

---
+++ salvacion/ejs/arrayEvenNum.js
@@ -0,0 +1,12 @@
+let sumEvenNumbers = (array) => {
+
+  let sum = 0;
+  for (let i =0; i< array.length; i++){
+    if (array[i] %2 !== 0){
+      sum+=array[i]*3;
+    }
+  }
+  console.log(sum);
+}
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+sumEvenNumbers(numbers);
