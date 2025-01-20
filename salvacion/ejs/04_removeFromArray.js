5ab02de3b65cca7745d928013b2531d4221211f8
diego <diegovscodee@gmail.com>
2025-01-20 19:52:22 +0100

    f

--- salvacion/ejs/04_removeFromArray.js
+++ salvacion/ejs/04_removeFromArray.js
@@ -1,5 +1,14 @@
 let removeFromArray = (array, number) => {
-  return array.filter(item => item !== number);
+  return (array.filter(checkSameNumber(array, number)));
 }
 
-removeFromArray([1, 2, 3, 4], 3); // [1, 2, 4]
+let checkSameNumber = (array, number)  => {
+  for (let i = 0; i<array.length; i++) {
+    if (array[i] === number) {
+      return true;
+    }
+  }
+  return number;
+}
+
+removeFromArray([1, 2, 3, 4], 3);
