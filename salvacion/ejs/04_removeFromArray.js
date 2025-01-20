4300d6fd459d27b744d406236d7824f2dc4e48a5
diego <diegovscodee@gmail.com>
2025-01-20 19:51:22 +0100

    Simplify removeFromArray function to directly filter out specified number

--- salvacion/ejs/04_removeFromArray.js
+++ salvacion/ejs/04_removeFromArray.js
@@ -1,14 +1,5 @@
 let removeFromArray = (array, number) => {
-  return (array.filter(checkSameNumber(array, number)));
+  return array.filter(item => item !== number);
 }
 
-let checkSameNumber = (array, number)  => {
-  for (let i = 0; i<array.length; i++) {
-    if (array[i] === number) {
-      return true;
-    }
-  }
-  return true;
-}
-
-removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3); // [1, 2, 4]
