ae20cc46ddb060a599323162358741decf40801b
diego <diegovscodee@gmail.com>
2025-01-20 19:49:44 +0100

    Refactor removeFromArray function to return filtered array and update checkSameNumber to return boolean

--- salvacion/ejs/04_removeFromArray.js
+++ salvacion/ejs/04_removeFromArray.js
@@ -1,15 +1,14 @@
 let removeFromArray = (array, number) => {
-  console.log(array.filter(checkSameNumber(array, number)));
-
+  return (array.filter(checkSameNumber(array, number)));
 }
 
 let checkSameNumber = (array, number)  => {
   for (let i = 0; i<array.length; i++) {
     if (array[i] === number) {
-      return number;
+      return true;
     }
   }
-  return number;
+  return true;
 }
 
 removeFromArray([1, 2, 3, 4], 3);
