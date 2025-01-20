33207d2f50ada011ef2a6fbc537e9416e87b9b76
diegosnchz <diegovscodee@gmail.com>
2025-01-20 11:39:11 +0100

    methods js -> .map (), .filter (), .reduce()

--- salvacion/ejs/arrayEvenNum.js
+++ salvacion/ejs/arrayEvenNum.js
@@ -1,11 +1,20 @@
-let sumEvenNumbers = (array) => {
-  let sum = 0;
-  for (let i =0; i< array.length; i++){
-    if (array[i] %2 !== 0){
-      sum+=array[i]*3;
-    }
-  }
-  console.log(sum);
+// let sumEvenNumbers = (array) => {
+//   let sum = 0;
+//   for (let i =0; i< array.length; i++){
+//     if (array[i] %2 === 0){
+//       sum+=array[i]*3;
+//     }
+//   }
+//   console.log(sum);
+// }
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// sumEvenNumbers(numbers);
+
+function sumOfTripledEvens(array) {
+  return console.log(array
+    .filter((num) => num % 2 === 0)
+    .map((num) => num * 3)
+    .reduce((acc, curr) => acc + curr));
 }
-const LAVIDAESUNATOMBOLATOMTOMTOMBOLA = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-sumEvenNumbers(LAVIDAESUNATOMBOLATOMTOMTOMBOLA);
+
+sumOfTripledEvens(numbers);
