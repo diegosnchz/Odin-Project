b9a466570013efed9e02a8a11cf381a4860f55a2
diego <diegovscodee@gmail.com>
2025-01-21 17:18:52 +0100

    Add Fibonacci sequence implementation and update script reference in HTML

---
+++ salvacion/ejs/10_fibonacci.js
@@ -0,0 +1,32 @@
+// A series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers. 
+// In this exercise, the Fibonacci sequence used is 1, 1, 2, 3, 5, 8, etc.
+
+
+// Fn = Fn-1 + Fn-2
+let fibonacci = (num) => {
+  let fibo = [1, 1, 2];
+  let contador = 0;
+
+  for (let i =0; i < num; i++) {
+    if (i > 2) {
+      let nextNum = fibo [i-1] + fibo [i-2];
+      fibo.push(nextNum);
+    } else {
+      contador += fibo[i]
+    }
+  }
+  return fibo[fibo.length-1];
+} 
+
+console.log(fibonacci(4)); // returns the 4th member of the series: 3  (1, 1, 2, 3)
+console.log(fibonacci(15)); // returns 8
+
+function fibonacci2(num) {
+  if (num === 0)
+    return (0);
+  if (num === 1)
+    return (1);
+  return (fibonacci2(num - 1) + fibonacci2(num - 2));
+}
+
+console.log(fibonacci2(15));
---
+++ salvacion/ejs/11_getTheTitles.js
@@ -0,0 +1,19 @@
+// You are given an array of objects that represent books with an author and a title that looks like this:
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
+
+let getTheTiles = (books) => {
+  
+}
+  
+
+// Your job is to write a function that takes the array and returns an array of titles:
+getTheTiles(books);
