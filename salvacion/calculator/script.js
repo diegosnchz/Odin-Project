65aacfe3b19e02b899877a3ff126522abb5301b7
diego <diegovscodee@gmail.com>
2025-01-23 17:35:12 +0100

    Refactor calculator logic to improve variable naming and streamline operation handling; update display function and event listeners for number input

--- salvacion/calculator/script.js
+++ salvacion/calculator/script.js
@@ -1,67 +1,8 @@
 let data = {
-  firstNum : 0,
+  num1 : '',
   operator : null,
-  secondNum : 0,
-  equals : 0
+  num2 : '',
 } 
-//DOM   
-
-//teclas numeros
-document.querySelector("#one").addEventListener('click', () => {
-  data.firstNum = 1;
-});
-
-document.querySelector("#two").addEventListener('click', () => {
-  data.firstNum = 2;
-});
-
-document.querySelector("#three").addEventListener('click', () => {
-  data.firstNum = 3;
-});
-
-document.querySelector("#four").addEventListener('click', () => {
-  data.firstNum = 4;
-});
-
-document.querySelector("#five").addEventListener('click', () => {
-  data.firstNum = 5;
-});
-
-document.querySelector("#six").addEventListener('click', () => {
-  data.firstNum = 6;
-});
-
-document.querySelector("#seven").addEventListener('click', () => {
-  data.firstNum = 7;
-});
-
-document.querySelector("#eight").addEventListener('click', () => {
-  data.firstNum = 8;
-});
-
-document.querySelector("#nine").addEventListener('click', () => {
-  data.firstNum = 9;
-});
-
-document.querySelector("#zero").addEventListener('click', () => {
-  data.firstNum = 0;
-});
-
-// teclas operadores
-document.querySelector("#divide").addEventListener('click', () => {
-  data.operator = '/';
-});
-document.querySelector("#multiply").addEventListener('click', () => {
-  data.operator = '*';
-});
-document.querySelector("#add").addEventListener('click', () => {
-  data.operator = '+';
-});
-document.querySelector("#subtract").addEventListener('click', () => {
-  data.operator = '-';
-});
-
-
 //FUNCIONES OPERACIONES
 const add = (num1, num2) => {
   return num1 + num2;
@@ -78,29 +19,36 @@ const multiply = (num1, num2) => {
 const divide = (num1, num2) => {
   return num1/num2;
 }
-//revisar fn operate
+
 const operate = (operator, num1, num2) => {
-  let addSum = add(num1, num2);
-  let substractSum = substract(num1, num2);
-  let divideSum = divide(num1, num2);
-  let multiplySum = multiply (num1, num2);
+  if (data.operator === 'add') {
+    return num1 + num2;
+  } else if (data.operator === 'substract') {
+    return num1 - num2;
+  } else if (data.operator === 'multiply') {
+    return num1 * num2;
+  } else {
+    return num1/num2;
+  }
 }
+
 //mostrar contenido en pantalla
 
-const updateDiplay = (num) => {
+const updateDisplay = (num) => {
   let display = document.querySelector("#display");
-  display.textContent = value;
+  display.textContent = num;
 }
 
 const handleNumberClick = (num) => {
   if (data.operator === null) {
-    data.num1 += num;
-    updateDiplay(data.firstNum)
+    data.num1 = parseFloat(data.num1 + num);
+    updateDisplay(data.num1);
   } else {
-    data.num2 += num;
-    updateDiplay(data.secondNum)
+    data.num2 = parseFloat(data.num2 + num);
+    updateDisplay(data.num2);
   }
 }
+
 
 //dom para el display
 document.querySelector("#one").addEventListener('click', () => handleNumberClick('1'));
@@ -127,3 +75,9 @@ document.querySelector("#add").addEventListener('click', () => {
 document.querySelector("#subtract").addEventListener('click', () => {
   data.operator = '-';
 });
+
+
+//revisar si está bien esto..
+document.querySelector("#equals").addEventListener("click", (num1, num2) => {
+  equals.textContent = operate(num1, num2);
+})
