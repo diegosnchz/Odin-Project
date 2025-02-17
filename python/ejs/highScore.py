93fed81bdf8bd108d897baf2cf1a7de985a8dac7
diego <diegovscodee@gmail.com>
2025-02-17 18:42:44 +0100

    Refactor high function to calculate word scores and return the highest scoring word; add new moving_zeros_to_the_end.py file

--- python/ejs/highScore.py
+++ python/ejs/highScore.py
@@ -1,12 +1,3 @@
-def high(x):
-    lowerX = lowerX.toLower()
-    listText = lowerX.split()
-    finalStr = ''
-
-    for i, char in enumerate(x):
-        
-
-
 def valorAbc():
     abc = "abcdefghijklmnopqrstuvwxyz"
     result = {}
@@ -14,3 +5,19 @@ def valorAbc():
         result[letter] = i + 1
     return result
 
+def high(x):
+    letter_values = valorAbc()
+    words = x.split()
+    word_scores = {}
+
+    for word in words:
+        score = 0
+        for letter in word:
+            score += letter_values.get(letter, 0)  
+        word_scores[word] = score
+
+    if not word_scores:
+        return ""  
+
+    best_word = max(word_scores, key=word_scores.get)
+    return best_word
